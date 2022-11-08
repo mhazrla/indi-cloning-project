ef4e62e93bc8bc7b894dd05bc761760320cd59f2
Azriel FachrulRezy <afachrulrezy@gmail.com>
2022-11-08 18:29:31 +0700

    add contact feature

--- resources/js/Pages/Contactus.jsx
+++ resources/js/Pages/Contactus.jsx
@@ -73,6 +73,29 @@ export default function Contactus(props) {
                                     >
                                         Submit
                                     </button>
+                                    {flash.success && (
+                                        <div className="alert alert-success shadow-lg">
+                                            <div>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    className="stroke-current flex-shrink-0 h-6 w-6"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth="2"
+                                                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                                    />
+                                                </svg>
+                                                <span>
+                                                    Your submit has been
+                                                    success!
+                                                </span>
+                                            </div>
+                                        </div>
+                                    )}
                                 </div>
                                 <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
                                     <a
